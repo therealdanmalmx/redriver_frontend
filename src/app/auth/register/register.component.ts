54225593494fa8550ca85cce6cafc6c7003be048
therealdanmalmx <danmalmx@gmail.com>
2024-12-26 14:19:44 +0000

    Adjust errro message

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -41,8 +41,8 @@ export class RegisterComponent {
           this.isSubmitting = false;
         }
       },
-      error: () => {
-        this.toastr.error('Could not register user.');
+      error: (error) => {
+        this.toastr.error(error.error.message);
         this.isSubmitting = false;
       },
     });
